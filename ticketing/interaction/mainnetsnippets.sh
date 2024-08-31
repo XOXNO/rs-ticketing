58149ed0f06ec56b31a1cef144839b14b1fe270f
Mihai Daniel Eremia <22687907+mihaieremia@users.noreply.github.com>
2024-08-31 14:55:39 +0300

    deploy mainnet

--- ticketing/interaction/mainnetsnippets.sh
+++ ticketing/interaction/mainnetsnippets.sh
@@ -1,16 +1,17 @@
-ADDRESS=
+ADDRESS=erd1qqqqqqqqqqqqqpgq03yjtslvm63d3whhunycqgyg923866c7ah0s9vht8j
 
-DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-devnet)
+DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-mainnet)
 PROXY=https://gateway.xoxno.com
 
 deploy() {
     echo ${PROJECT}
-    mxpy --verbose contract deploy --metadata-payable --bytecode="/Users/mihaieremia/GitHub/rs-ticketing/ticketing/output/minter.wasm" --recall-nonce  --gas-limit=100000000 \
+    mxpy --verbose contract deploy --metadata-payable --bytecode="/Users/mihaieremia/GitHub/rs-ticketing/ticketing/output/ticketing.wasm" --recall-nonce  --gas-limit=500000000 \
+    --arguments 0x96 \
     --send --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 --proxy=${PROXY} --chain=1 || return
 }
 
 upgrade() {
-    mxpy --verbose contract upgrade ${ADDRESS} --metadata-payable --bytecode="/Users/mihaieremia/GitHub/rs-ticketing/ticketing/output/minter.wasm" \
+    mxpy --verbose contract upgrade ${ADDRESS} --metadata-payable --bytecode="/Users/mihaieremia/GitHub/rs-ticketing/ticketing/output/ticketing.wasm" \
     --recall-nonce --recall-nonce --ledger --ledger-account-index=0 --ledger-address-index=0 \
-    --gas-limit=125000000 --send --outfile="upgrade.json" --proxy=${PROXY} --chain=1 || return
+    --gas-limit=150000000 --send --outfile="upgrade.json" --proxy=${PROXY} --chain=1 || return
 }
